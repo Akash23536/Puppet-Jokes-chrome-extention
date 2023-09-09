97cd35b8d7787f0f46b379f49d4282ce19695969
Akash Bhadana <23536akash.2021@gmail.com>
2023-09-09 16:06:49 +0530

    hindi jokes url added

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-fetch('https://icanhazdadjoke.com/slack')
+// https://icanhazdadjoke.com/slack           // english jokes url
+// https://hindi-jokes-api.onrender.com/jokes?api_key=1f2f44b6d56ff4f906c94e61a69f             // hindi jokes url 
+
+fetch('https://hindi-jokes-api.onrender.com/jokes?api_key=1f2f44b6d56ff4f906c94e61a69f') 
 .then(data=>data.json())
 .then(jokeData =>{
-const jokeText=jokeData.attachments[0].text;
+// const jokeText=jokeData.attachments[0].text;              //for english url
+const jokeText=jokeData.jokeContent;
 const jokeElement = document.getElementById('jokeElement');
 jokeElement.innerHTML = jokeText;
 
