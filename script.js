58dfb042fd99239e89e56900fea490de7ba5a79a
Akash Bhadana <23536akash.2021@gmail.com>
2023-09-10 21:44:37 +0530

    lan buttons added and both api call working

--- script.js
+++ script.js
@@ -1,65 +1,60 @@
-// fetch('https://icanhazdadjoke.com/slack')  // english jokes url
-// .then(data=>data.json())
-// .then(jokeData =>{
-// const jokeText=jokeData.attachments[0].text;              //for english url
-// const jokeElement = document.getElementById('jokeElement');
-// jokeElement.innerHTML = jokeText;
-// })
-
-async function fetchAndDisplayJoke() {
+async function fetchAndDisplayEnglishJoke() {
     try {
-      // Fetch the joke data
-      const response = await fetch('https://icanhazdadjoke.com/slack');
-      if (!response.ok) {
-        throw new Error('Failed to fetch joke');
-      }
-      // Parse the JSON response
-      const jokeData = await response.json();
-      // Extract the joke text
-      const jokeText = jokeData.attachments[0].text;
-      // Get the HTML element
-      const jokeElement = document.getElementById('jokeElement');
-      // Display the joke text on the web page
-      jokeElement.innerHTML = jokeText;
+        // Fetch the joke data
+        const response = await fetch('https://icanhazdadjoke.com/slack');
+        if (!response.ok) { throw new Error('Failed to fetch joke'); }
+        // Parse the JSON response
+        const jokeData = await response.json();
+        // Extract the joke text
+        const jokeText = jokeData.attachments[0].text;
+        // Get the HTML element
+        const jokeElement = document.getElementById('jokeElement');
+        // Display the joke text on the web page
+        jokeElement.innerHTML = jokeText;
     } catch (error) {
-      console.error(error);
+        console.error(error);
     }
-  }
-  // Call the async function to fetch and display the joke
-  fetchAndDisplayJoke();
-
-// ---------------------------------------------------------------------------------------------------
-
-
-// fetch('https://hindi-jokes-api.onrender.com/jokes?api_key=1f2f44b6d56ff4f906c94e61a69f')    // hindi jokes url 
-// .then(data=>data.json())
-// .then(jokeData =>{
-// const jokeText=jokeData.jokeContent;      //for hindi jokes url
-// const jokeElement = document.getElementById('jokeElement');
-// jokeElement.innerHTML = jokeText;
-// })
-
-
+}
 
 async function fetchAndDisplayHindiJoke() {
     try {
-      // Fetch the Hindi joke data
-      const response = await fetch('https://hindi-jokes-api.onrender.com/jokes?api_key=1f2f44b6d56ff4f906c94e61a69f');
-      if (!response.ok) {
-        throw new Error('Failed to fetch Hindi joke');
-      }
-      // Parse the JSON response
-      const jokeData = await response.json();
-      // Extract the joke text
-      const jokeText = jokeData.jokeContent;
-      // Get the HTML element
-      const jokeElement = document.getElementById('jokeElement');
-      // Display the joke text on the web page
-      jokeElement.innerHTML = jokeText;
+        // Fetch the Hindi joke data
+        const response = await fetch('https://hindi-jokes-api.onrender.com/jokes?api_key=1f2f44b6d56ff4f906c94e61a69f');
+        if (!response.ok) { throw new Error('Failed to fetch Hindi joke'); }
+        // Parse the JSON response
+        const jokeData = await response.json();
+        // Extract the joke text
+        const jokeText = jokeData.jokeContent;
+        // Get the HTML element
+        const jokeElement = document.getElementById('jokeElement');
+        // Display the joke text on the web page
+        jokeElement.innerHTML = jokeText;
     } catch (error) {
-      console.error(error);
+        console.error(error);
     }
-  }
-  // Call the async function to fetch and display the Hindi joke
-  fetchAndDisplayHindiJoke();
-  
+}
+
+fetchAndDisplayEnglishJoke();
+
+// Get a reference to the Language button by its ID
+const languageEnglishButton = document.getElementById('RightLanguageButton');
+// Define an onclick function for the Language button
+languageEnglishButton.onclick = function () {
+    // Call the fetchAndDisplayHindiJoke function when the Language button is clicked
+ // Call the async function to fetch and display the joke by default on first run
+fetchAndDisplayEnglishJoke();
+};
+
+
+// Get a reference to the Language button by its ID
+const languageHindiButton = document.getElementById('leftLanguageButton');
+// Define an onclick function for the Language button
+languageHindiButton.onclick = function () {
+    // Call the fetchAndDisplayHindiJoke function when the Language button is clicked
+    // Call the async function to fetch and display the Hindi joke
+    fetchAndDisplayHindiJoke();
+};
+
+
+
+
